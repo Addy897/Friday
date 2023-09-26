e58d941416d3cc9c69af25444839fd18596fd26b
Addy897 <145797684+Addy897@users.noreply.github.com>
2023-09-26 18:48:43 +0530

    Update Assistant.py

--- Assistant.py
+++ Assistant.py
@@ -87,7 +87,7 @@ class Friday():
                                         rep=random.choice(library().leaving)
                                         self.speak(rep)
                                         exit(0)
-			elif("what is your name" in cmd.lower())
+			elif("what is your name" in cmd.lower() or "who are you" in cmd.lower())
 				rep=f"my name is {self.name}" 
 				self.speak(rep)
                         elif("play" in cmd and "song" in cmd):
